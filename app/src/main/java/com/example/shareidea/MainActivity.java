a83a761699721601f6ac1842f943e7cbb46b34ec
지민경 <63912449+jjhair119@users.noreply.github.com>
2020-08-27 23:14:48 +0900

    08271114

--- app/src/main/java/com/example/shareidea/MainActivity.java
+++ app/src/main/java/com/example/shareidea/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.shareidea.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
--- app/src/main/java/com/example/shareidea/RegisterActivity.java
+++ app/src/main/java/com/example/shareidea/RegisterActivity.java
@@ -9,20 +9,9 @@ import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private Button registerbtn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        registerbtn = (Button) findViewById(R.id.btn_login_signup);
-
-        registerbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-            }
-        });
     }
 }
