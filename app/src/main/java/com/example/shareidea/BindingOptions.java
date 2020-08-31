f881de7b9ee6c46942fe3b43865a9e09ad6f91a7
roian6 <roian6@naver.com>
2020-09-01 03:30:04 +0900

    메모완성

---
+++ app/src/main/java/com/example/shareidea/BindingOptions.java
@@ -0,0 +1,16 @@
+package com.example.shareidea;
+
+import android.widget.Toast;
+
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.ObservableArrayList;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BindingOptions {
+
+    @BindingAdapter("memoItem")
+    public static void bindMemoItem(RecyclerView recyclerView, ObservableArrayList<MemoModel> items) {
+        MemoAdapter adapter = (MemoAdapter) recyclerView.getAdapter();
+        if (adapter != null) adapter.setItem(items);
+    }
+}
--- app/src/main/java/com/example/shareidea/SplashActivity.java
+++ app/src/main/java/com/example/shareidea/SplashActivity.java
@@ -6,27 +6,33 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+
 import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.WindowManager;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getWindow().setStatusBarColor(Color.parseColor("#fbfbfb"));
 
-        View view = getWindow().getDecorView();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (view != null) {
-                view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-                getWindow().setStatusBarColor(Color.parseColor("#fbfbfb"));
-            }
-        }
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            //자동로그인 가능!
+            if (firebaseAuth.getCurrentUser() != null && UserCache.getUser(this) != null)
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
 
-        new Handler().postDelayed(() -> {
-            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                //자동로그인 불가능
+            else
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+
             finish();
         }, 1500);
     }
